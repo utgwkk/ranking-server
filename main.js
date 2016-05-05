e9c486ba0d5ae34df0cef8513bd9d8c66eaa3492
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-05-06 02:15:24 +0900

    Get ranking from DB

--- main.js
+++ main.js
@@ -16,12 +16,54 @@ app.use(_.get('/', function *() {
     this.body = "<h1>It works!</h1>";
 }));
 
+function *rankingQuery (name) {
+    return new Promise(function (resolve, reject) {
+        const db = new sqlite3.Database(config.database_path);
+        db.serialize(function () {
+            db.all('SELECT player_name AS name, point FROM scores WHERE game_name = $name ORDER BY point DESC', {$name: name}, function (err, rows) {
+                db.close();
+                resolve(rows || []);
+            });
+        });
+    });
+}
+
+function tokenQuery (name) {
+    return new Promise(function (resolve, reject) {
+        const db = new sqlite3.Database(config.database_path);
+        db.serialize(function () {
+            db.get('SELECT token FROM games WHERE name = $name', {$name: name}, function (err, row) {
+                db.close();
+                resolve(row && row.token || '');
+            });
+        });
+    });
+}
+
 app.use(_.get('/:name', function *(name) {
+    const token = this.request.query.token || '';
+    const validToken = yield tokenQuery(name);
     this.headers['Content-Type'] = 'text/plain';
-    const result = {
-        name
-    };
-    this.body = JSON.stringify(result);
+    let result;
+    if (validToken != '' && token == validToken) {
+        let ranking = [];
+        result = {
+            name,
+            ranking: yield rankingQuery(name),
+        };
+    } else {
+        let errorMessage;
+        if (validToken == '') {
+            errorMessage = 'The game ' + name + ' is not registered.';
+        } else if (token != validToken) {
+            errorMessage = 'Invalid token';
+        }
+        result = {
+            error: errorMessage
+        };
+        this.status = 400;
+    }
+    this.body = result;
 }));
 
 app.listen(3000);
